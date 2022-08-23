17be7ea8abf823de04e63eb944a32e420b3d35bd
Oliver Newland <oliver.newland@sentry.io>
2022-08-23 13:58:58 -0700

    major scale detection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::cmp::max;
 use std::io::stdin;
 // use std::process::Command;
-use std::{error::Error};
+use std::error::Error;
 
 use midir::{Ignore, MidiInput};
 
@@ -9,13 +10,16 @@ const KEY_UP: u8 = 128;
 const KEEP_ALIVE: u8 = 254;
 const TIME_KEEPING: u8 = 208;
 
-#[derive(Debug)]
+const BREAK_DELAY_MICROSECONDS: u64 = 4_000_000;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum MidiMessageTypes {
     KeyDown = 144,
     KeyUp = 128,
     KeepAlive = 254,
 }
 
+#[derive(Clone, Copy, Debug)]
 struct KeyMessage {
     timestamp: u64,
     message_type: MidiMessageTypes,
@@ -30,8 +34,95 @@ impl KeyMessage {
         let str_note = keys
             .get((usize::from(self.key) + NOTE_SEQ_OFFSET) % keys.len())
             .unwrap();
-        return format!("{}{}", str_note, self.key/12);
+        return format!("{}{}", str_note, self.key / 12);
     }
+
+    fn print(&self) {
+        print!("{:?}{} ", self.message_type, self.readable_note());
+    }
+}
+
+#[derive(Clone)]
+struct KeyBuffer {
+    buf: Vec<KeyMessage>,
+    most_recent_insert: u64,
+    // run_end_listeners: Vec<Box<dyn RunEndListener>>,
+}
+
+trait RunEndListener {
+    fn on_run_end(&self, buf: Vec<KeyMessage>);
+}
+
+impl KeyBuffer {
+    fn accept(&mut self, message: KeyMessage) {
+        if message.timestamp - self.most_recent_insert > BREAK_DELAY_MICROSECONDS {
+            println!("new run");
+            self.buf.clear();
+        }
+        self.buf.push(message);
+        self.most_recent_insert = max(message.timestamp, self.most_recent_insert);
+    }
+
+    fn print(&self) {
+        print!(
+            "KeyBuffer [ most_recent_insert = {} ] [ keys = ",
+            self.most_recent_insert
+        );
+        for msg in &self.buf {
+            msg.print()
+        }
+        println!("]")
+    }
+}
+
+fn is_ascending_major_scale(key_events: &Vec<KeyMessage>) -> bool {
+    // there should be [multiple of] 16 key-down then up events,
+    // for 8 notes played and then lifted
+    if key_events.len() < 16 || key_events.len() % 16 != 0 {
+        return false;
+    }
+
+    // an ascending major scale is the following sequence of key
+    // down followed by up of the same note with no overlap.
+    //
+    // whole [step] - whole - half - whole - whole - whole - half
+    let proper_deltas = vec![2, 2, 1, 2, 2, 2, 1];
+    let mut pair_based_index = 0;
+    let mut base_note = key_events[0].key;
+
+    // go pair by pair
+    while pair_based_index < key_events.len() / 2 {
+        let granular_index = pair_based_index * 2;
+
+        // enforce down then up
+        if key_events[granular_index].message_type != MidiMessageTypes::KeyDown {
+            return false;
+        }
+        if key_events[granular_index + 1].message_type != MidiMessageTypes::KeyUp {
+            return false;
+        }
+
+        // enforce same key down then up
+        if key_events[granular_index].key != key_events[granular_index + 1].key {
+            return false;
+        }
+
+        // if not on the first pair, make sure we're moving up by correct number of steps
+        if pair_based_index > 0
+            && (key_events[granular_index].key - base_note != proper_deltas[(pair_based_index - 1) % 8])
+        {
+            return false;
+        } else {
+            // this pair is good, on to the next one and update the base note
+            base_note = key_events[granular_index].key
+        }
+        pair_based_index += 1
+    }
+    if key_events[0].message_type != MidiMessageTypes::KeyDown {
+        return false;
+    }
+
+    return true;
 }
 
 // real pianos start with a low A, the midi standard starts at C
@@ -71,7 +162,10 @@ fn run() -> Result<(), Box<dyn Error>> {
     println!("\nOpening connection");
     let _in_port_name = midi_in.port_name(in_port)?;
     let known_message_types = vec![KEY_DOWN, KEY_UP, KEEP_ALIVE, TIME_KEEPING];
-
+    let mut buf = KeyBuffer {
+        most_recent_insert: 0,
+        buf: Vec::new(),
+    };
     // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
     let _conn_in = midi_in.connect(
         in_port,
@@ -88,12 +182,9 @@ fn run() -> Result<(), Box<dyn Error>> {
             if message.len() == 3 {
                 if message[0] == KEY_UP || message[0] == KEY_DOWN {
                     let parsed_message = build_key_message(stamp, message);
-                    println!(
-                        "{:?} {:?}: {}",
-                        parsed_message.timestamp,
-                        parsed_message.message_type,
-                        parsed_message.readable_note()
-                    );
+                    buf.accept(parsed_message);
+                    buf.print();
+                    println!("is major scale: {}", is_ascending_major_scale(&buf.buf))
                     // sentry::capture_message(format!("user played {str_note}").as_str(), sentry::Level::Info);
                 }
             }
